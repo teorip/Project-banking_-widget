e56a2ab3daa13f51c4370f20cb2efdf296a4bea2
Mikhail <mikhail.sisoev@gmail.com>
2025-04-06 14:29:07 +0300

    добавлена функция mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,23 @@
 import datetime
+from masks import get_mask_card_number
+from masks import get_mask_account
 
 
 def get_data(data_string: str) -> str:
-    """Функция преобразовния строки из формата "2024-03-11T02:26:18.671407"в формат "ДД.ММ.ГГГГ" """
+    """Функция преобразовния строки из формата "2024-03-11T02:26:18.671407"
+    в формат "ДД.ММ.ГГГГ" """
     date_format = datetime.datetime.strptime(data_string, "%Y-%m-%dT%H:%M:%S.%f")
     return date_format.strftime("%d.%m.%Y")
+
+
+def mask_account_card(bank_details: str) -> str:
+    """ """
+    list_bank_details = bank_details.split(" ")
+    if len(list_bank_details[-1]) == 16:
+        processed_data = f"{" ".join(list_bank_details[0:-1])} {get_mask_card_number(list_bank_details[-1])}"
+    elif len(list_bank_details[-1]) == 18:
+        processed_data = f"{" ".join(list_bank_details[0:-1])} {get_mask_account(list_bank_details[-1])}"
+    return processed_data
+
+
+
