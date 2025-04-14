641b84a8b4ceb56800039227a885582aecaef608
Mikhail <mikhail.sisoev@gmail.com>
2025-04-14 12:07:27 +0300

    in processing.py add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
-from typing import Dict, List
+from collections.abc import Callable
+from typing import Any, Dict, List
 
 
 def filter_by_state(
@@ -7,3 +8,13 @@ def filter_by_state(
     """Функция фильтрации словарей по значению ключа"""
 
     return [item for item in bank_transaction_data if item.get("state") == state]
+
+
+def sort_by_date(
+    bank_transaction_data: List[Dict[str, Any]], reverse: bool = True
+) -> List:
+    """Функция фильтрации словарей по дате"""
+
+    return sorted(
+        bank_transaction_data, key=lambda item: item.get("date"), reverse=reverse
+    )
