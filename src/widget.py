30190c986db09bee421b12c930f7c804a2fa2359
Mikhail <mikhail.sisoev@gmail.com>
2025-04-26 14:40:25 +0300

    adding a condition ValueError

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 import datetime
+from typing import Union
 
 from src import masks
 
@@ -6,16 +7,21 @@ from src import masks
 def get_data(data_string: str) -> str:
     """Функция преобразовния строки из формата "2024-03-11T02:26:18.671407"
     в формат "ДД.ММ.ГГГГ" """
+
     date_format = datetime.datetime.strptime(data_string, "%Y-%m-%dT%H:%M:%S.%f")
+
     return date_format.strftime("%d.%m.%Y")
 
 
-def mask_account_card(bank_details: str) -> str:
+def mask_account_card(bank_details: Union[str, int]) -> str:
     """Функция маскирования карт и счетов клиента.
     Использован импорт функций их файла masks"""
+    bank_details = str(bank_details)
     list_bank_details = bank_details.split(" ")
     if len(list_bank_details[-1]) == 16:
         processed_data = f"{" ".join(list_bank_details[0:-1])} {masks.get_mask_card_number(list_bank_details[-1])}"
     elif len(list_bank_details[-1]) == 20:
         processed_data = f"{" ".join(list_bank_details[0:-1])} {masks.get_mask_account(list_bank_details[-1])}"
+    elif len(list_bank_details[-1]) < 16 or len(list_bank_details[-1]) < 20:
+        raise ValueError("Введены не корректные данные")
     return processed_data
