4e76efa146e09ddcdafd46aadac78ec687df2253
Mikhail <mikhail.sisoev@gmail.com>
2025-04-27 15:58:52 +0300

    add test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,24 +1,26 @@
-import pytest
 from typing import Union
+
+import pytest
 
 from src.widget import get_data, mask_account_card
 
 
 @pytest.fixture
-def data(type_date: Union[str, int]) -> str:
+def data() -> str|int:
     return format("%Y-%m-%dT%H:%M:%S.%f")
 
 
 @pytest.mark.parametrize(
     "data, expected_result", [("2024-03-11T02:26:18.671407", "11.03.2024")]
 )
-def test_get_data(data: str, expected_result: str)-> None:
+def test_get_data(data: str, expected_result: str) -> None:
     assert get_data(data) == expected_result
 
 
-def test_get_data_invalid_data(data: str) -> None:
+def test_get_data_invalid_data(data: str|int) -> None:
     with pytest.raises(ValueError):
-        get_data("invalid_date")
+        result = "invalid_data"
+        get_data (result)
 
 
 def test_get_data_absent_of_date(data: str) -> None:
@@ -27,13 +29,13 @@ def test_get_data_absent_of_date(data: str) -> None:
 
 
 @pytest.fixture
-def bank_details(type_bank_details: int) -> int:
+def bank_details() -> int:
     return 73654108430135874306
 
 
 @pytest.mark.parametrize(
     "bank_details, expected_result",
-    [(73654108430135874306, " **4306"), (7365410843013587, " 7365 41** **** 3587")],
+    [("73654108430135874306", " **4306"), ("7365410843013587", " 7365 41** **** 3587")],
 )
 def test_mask_account_card(bank_details: int, expected_result: str) -> None:
     assert mask_account_card(bank_details) == expected_result
