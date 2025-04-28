99e84a828274964343dbbf7cd39222b18421fba7
Mikhail <mikhail.sisoev@gmail.com>
2025-04-28 15:01:19 +0300

    refactoring code

--- src/masks.py
+++ src/masks.py
@@ -2,24 +2,23 @@ def get_mask_card_number(card_number: int) -> str:
     """Принимает на вход номер карты и возвращает ее маску."""
 
     card_str = str(card_number)
-    if len(card_str) != 16:
-        raise ValueError("Номер карты должен содержать ровно 16 цифр.")
-
-    masked_card = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
-
-    return masked_card
+    if card_str.isnumeric() and len(card_str) == 16:
+        masked_card = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
+        return masked_card
+    else:
+        raise ValueError("Номер карты должен содержать только цифры и равен 16 цифрам")
 
 
 def get_mask_account(account_number: int) -> str:
     """Принимает на вход номер счета и возвращает его маску."""
 
     account_str = str(account_number)
-    if len(account_str) != 20:
-        raise ValueError("Номер счёта должен содержать ровно 20 цифр.")
+    if account_str.isnumeric() and len(account_str) == 20:
+        masked_account = f"**{account_str[-4:]}"
+        return masked_account
+    else:
+        raise ValueError("Номер счёта должен содержать только цифры и равен 20 цифрам.")
 
-    masked_account = f"**{account_str[-4:]}"
-
-    return masked_account
 
 if __name__ == "__main__":
     print()
