e0a3d0792458c297ff605a524698c5060441197d
Mikhail <mikhail.sisoev@gmail.com>
2025-05-03 10:42:04 +0300

    A report on the module's test coverage has been created generators.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, List
 import pytest
 
 
+# Фикстуры для задания 10.2
 @pytest.fixture
 def card_number() -> int:
     return 7000792289606361
@@ -84,3 +85,84 @@ def list_data_false() -> List[Dict]:
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
     ]
+
+
+# Фикстуры для задания 11.1
+@pytest.fixture
+def by_currency() -> list:
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {"name": "руб.", "code": "RUB"},
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160",
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {"name": "USD", "code": "USD"},
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {"name": "руб.", "code": "RUB"},
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
+    ]
+
+
+@pytest.fixture
+def empty_by_currency() -> list:
+    return []
+
+
+@pytest.fixture
+def number_no_diapason() -> int:
+    return 10000000000000000
+
+
+if __name__ == "__main__":
+    print()
